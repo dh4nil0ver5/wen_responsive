feb7472088feae0737b1913e877d5a0aa8ecb596
dh4nil0ver5 <kokme.km@gmail.com>
2022-01-24 15:16:10 +0700

    "add new files"

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,35 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:pjj_apps/CustomTab.dart';
+import 'package:pjj_apps/CustomTabBar.dart';
 import 'package:pjj_apps/content_view.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key); 
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> { 
-
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
   List<ContentView> contentViews = [
-    ContentView(tab: CustomTab(title: 'Home'), content: Center(child: Container(color: Colors.green, width: 100, height: 100),),),
+    ContentView(
+      tab: CustomTab(title: 'Home'),
+      content: Center(
+        child: Container(color: Colors.green, width: 100, height: 100),
+      ),
+    ),
+    ContentView(
+      tab: CustomTab(title: 'Service'),
+      content: Center(
+        child: Container(color: Colors.green, width: 100, height: 100),
+      ),
+    ),
+    ContentView(
+      tab: CustomTab(title: 'Info'),
+      content: Center(
+        child: Container(color: Colors.green, width: 100, height: 100),
+      ),
+    ),
   ];
   @override
-  Widget build(BuildContext context) { 
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = TabController(length: contentViews.length, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( 
+      appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: LayoutBuilder(
-        builder: (context, constraints){
-          if (constraints.maxWidth>715) {
-            return desktopView();
-          } else {
-            return mobileView();
-          }
+      body: LayoutBuilder(builder: (context, constraints) {
+        if (constraints.maxWidth > 715) {
+          return desktopView();
+        } else {
+          return mobileView();
         }
-      ),
+      }),
     );
   }
 
@@ -42,6 +66,10 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget desktopView() {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[CustomTabBar(controller: tabController, tabs: contentViews.map((e) => e.tab).toList()), Container()],
+    );
   }
 }
